0c775e27bada579631329fc994700084a234488e
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-04-04 21:30:40 -0500

    Agrega recursos de video

--- app/src/main/java/com/example/practicavideo/MainActivity.kt
+++ app/src/main/java/com/example/practicavideo/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.practicavideo
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
 import androidx.appcompat.app.AppCompatActivity
+import com.example.practicavideo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(binding.reproductor)
+
+        val contenido: Uri =Uri.parse("android.resource://$packageName/${R.raw.primero}")
+        binding.reproductor.setVideoURI(contenido)
+        binding.reproductor.requestFocus()
+        binding.reproductor.start()
     }
 }
